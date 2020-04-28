60134e4c8267016c80ef6a4211ee49aa94d42687
Love Mowitz <love.mowitz@gmail.com>
2020-04-28 22:37:12 +0200

    Add base widget

---
+++ BusMonitorWidget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  BusMonitorWidget
+//
+//  Created by Love Mowitz on 2020-04-28.
+//  Copyright © 2020 Love Mowitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
