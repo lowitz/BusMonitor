d2ba095b1c3b3e6e56ea9d282ecfbb7c2be66190
Love Mowitz <love.mowitz@gmail.com>
2020-05-05 20:05:08 +0200

    Mark turn switch on if stop is in favorite list

--- BusMonitor/StopLocation.mm
+++ BusMonitor/StopLocation.mm
@@ -14,6 +14,27 @@
 @implementation StopLocation
 
 - (bool)isFavorite {
+    NSUserDefaults *defaults = [[NSUserDefaults standardUserDefaults] initWithSuiteName:@"group.com.lovemowitz.BusMonitor"];
+    NSData *data = [defaults objectForKey:@"stopLocationFavorites"];
+
+    NSMutableArray* favoriteStops = nil;
+    if (data != nil) {
+        NSMutableArray *tempArray = [[NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:NSMutableArray.class, StopLocation.class, nil] fromData:data error:nil] mutableCopy];
+        if (tempArray) {
+            favoriteStops = [[NSMutableArray alloc] initWithArray:tempArray];
+        } else {
+            favoriteStops = [[NSMutableArray alloc] init];
+        }
+    } else {
+        favoriteStops = [[NSMutableArray alloc] init];
+    }
+
+    for (int i = 0; i < [favoriteStops count]; i++) {
+        auto stop = static_cast<StopLocation*>(favoriteStops[i]);
+        if (stop.stopID == self.stopID) {
+            return true;
+        }
+    }
     return false;
 }
 
