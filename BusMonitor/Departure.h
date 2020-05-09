818468141bc6c1c534f7aed6e8a53ce2d83646c8
Love Mowitz <love.mowitz@gmail.com>
2020-05-09 13:13:29 +0200

    Add departure class

---
+++ BusMonitor/Departure.h
@@ -0,0 +1,37 @@
+//
+//  Departure.h
+//  BusMonitor
+//
+//  Created by Love Mowitz on 2020-05-09.
+//  Copyright © 2020 Love Mowitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Departure : NSObject
+
+// Sign-name and color
+@property (strong, nonatomic) NSString *sname; // Sign-name?
+@property (strong, nonatomic) NSString *stroke; // Brush?
+@property (strong, nonatomic) NSString *bgColor; // UIColor?
+@property (strong, nonatomic) NSString *fgColor; // UIColor?
+
+// What stop, departure name, type, track, direction and accessibility
+@property (nonatomic) NSInteger stopID;
+@property (strong, nonatomic) NSString *stopName;
+@property (strong, nonatomic) NSString *departureName; // Full name
+@property (strong, nonatomic) NSString *type; // Bus, tram, etc.
+@property (strong, nonatomic) NSString *track;
+@property (strong, nonatomic) NSString *direction;
+@property (strong, nonatomic) NSString *accessibility;
+
+// Departure time
+@property (strong, nonatomic) NSString *rtTime; // rt? Is this the estimated time?
+@property (strong, nonatomic) NSString *time;
+
+// Journey details
+@property (nonatomic) NSInteger journeyNumber;
+@property (nonatomic) NSInteger journeyID; // Wtf is journey number then?
+@property (strong, nonatomic) NSString *journeyDetailRef;
+
+@end
---
+++ BusMonitor/Departure.mm
@@ -0,0 +1,13 @@
+//
+//  Departure.m
+//  BusMonitor
+//
+//  Created by Love Mowitz on 2020-05-09.
+//  Copyright © 2020 Love Mowitz. All rights reserved.
+//
+
+#import "Departure.h"
+
+@implementation Departure
+
+@end
